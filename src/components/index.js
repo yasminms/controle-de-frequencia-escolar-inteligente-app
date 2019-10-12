b9aca036db8f30d69d2a3dadf5e4c17f1b14c68d
Yasmin Moraes da Silva <yasminms46@gmail.com>
2019-10-12 18:55:55 -0300

    feat: adicionando botão padrão

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,8 @@ import { MaskInput } from './shared/mask-input/mask-input';
 import { Label } from './shared/label/label';
 import { Radio } from './shared/radio/radio';
 import { Select } from './shared/select/select';
+import { Button } from './shared/button/button'
 
 export {
-  Input, MaskInput, Label, Radio, Select,
+  Input, MaskInput, Label, Radio, Select, Button,
 };
