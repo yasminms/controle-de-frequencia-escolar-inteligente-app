668bff1c92f82efb0b925ea8687d9a289eaf0544
Yasmin Moraes da Silva <yasminms46@gmail.com>
2019-11-08 10:33:22 -0300

    feat(class-register): adicionando botões

--- src/scenes/class-register/class-register.jsx
+++ src/scenes/class-register/class-register.jsx
@@ -7,6 +7,7 @@ import {
   Label,
   InputTime,
   Row,
+  Button,
 } from '@/components'
 import { findAllGroups } from '@/services/group-service'
 import { findAllClassrooms } from '@/services/classroom-service'
@@ -68,217 +69,243 @@ export const ClassRegister = () => {
   return (
     <div className='class-register'>
       <div className='class-register__container'>
-        <div className='class-register__container__column'>
-          <div className='class-register__container__column__form-group'>
-            <Label text='Turma' htmlFor='group' />
-            <Select onChange={e => setGroup(e.target.value)} value={group}>
-              {mapGroupsToOptions()}
-            </Select>
-          </div>
-          <div className='class-register__container__column__form-group'>
-            <Label text='Disciplina' htmlFor='subject' />
-            <Select onChange={e => setSubject(e.target.value)} value={subject}>
-              {mapSubjectsToOptions()}
-            </Select>
-          </div>
-          <div className='class-register__container__column__form-group'>
-            <Label text='Sala de aula' htmlFor='classroom' />
-            <Select
-              onChange={e => setClassroom(e.target.value)}
-              value={classroom}
-            >
-              {mapClassroomsToOptions()}
-            </Select>
-          </div>
-          <Row>
-            <div className='class-register__container__column__form-group  class-register__container__column__first-element-row'>
-              <Label text='Ano' htmlFor='year' />
-              <MaskInput
-                type='text'
-                mask='9999'
-                value={year}
-                onChange={e => setYear(e.target.value)}
-                name='year'
-              />
+        <Row>
+          <div className='class-register__container__column'>
+            <div className='class-register__container__column__form-group'>
+              <Label text='Turma' htmlFor='group' />
+              <Select onChange={e => setGroup(e.target.value)} value={group}>
+                {mapGroupsToOptions()}
+              </Select>
             </div>
             <div className='class-register__container__column__form-group'>
-              <Label text='Tolerância' htmlFor='tolerance' />
-              <InputTime
-                inputComponent={<Input />}
-                value={tolerance}
-                onChange={e => setTolerance(e.target.value)}
-              />
+              <Label text='Disciplina' htmlFor='subject' />
+              <Select
+                onChange={e => setSubject(e.target.value)}
+                value={subject}
+              >
+                {mapSubjectsToOptions()}
+              </Select>
             </div>
-          </Row>
-        </div>
-        <div className='class-register__container__column'>
-          <div className='class-register__container__column__form-group'>
-            <Label text='Dia da semana e início da aula' htmlFor='dayOfWeek' />
-            <div className='class-register__container__column__item'>
-              <Checkbox
-                checked={classList[0].isDaySelected}
-                onChange={() =>
-                  listHandleChange(
-                    0,
-                    !classList[0].isDaySelected,
-                    'isDaySelected'
-                  )
-                }
-                text='Segunda-feira'
-                name='monday'
-              />
-              <div className='class-register__container__column__item__special-input'>
-                <InputTime
-                  inputComponent={<Input />}
-                  value={classList[0].startTime}
-                  onChange={e =>
-                    listHandleChange(0, e.target.value, 'startTime')
-                  }
-                />
-              </div>
-              <div className='class-register__container__column__item__normal-input'>
+            <div className='class-register__container__column__form-group'>
+              <Label text='Sala de aula' htmlFor='classroom' />
+              <Select
+                onChange={e => setClassroom(e.target.value)}
+                value={classroom}
+              >
+                {mapClassroomsToOptions()}
+              </Select>
+            </div>
+            <Row>
+              <div className='class-register__container__column__form-group  class-register__container__column__first-element-row'>
+                <Label text='Ano' htmlFor='year' />
                 <MaskInput
                   type='text'
-                  mask='99'
-                  value={classList[0].periods}
-                  onChange={e => listHandleChange(0, e.target.value, 'periods')}
-                  placeholder='Períodos'
-                  name='mondayPeriods'
+                  mask='9999'
+                  value={year}
+                  onChange={e => setYear(e.target.value)}
+                  name='year'
                 />
               </div>
-            </div>
-            <div className='class-register__container__column__item'>
-              <Checkbox
-                checked={classList[1].isDaySelected}
-                onChange={() =>
-                  listHandleChange(
-                    1,
-                    !classList[1].isDaySelected,
-                    'isDaySelected'
-                  )
-                }
-                text='Terça-feira'
-                name='tuesday'
-              />
-              <div className='class-register__container__column__item__special-input'>
+              <div className='class-register__container__column__form-group'>
+                <Label text='Tolerância' htmlFor='tolerance' />
                 <InputTime
                   inputComponent={<Input />}
-                  value={classList[1].startTime}
-                  onChange={e =>
-                    listHandleChange(1, e.target.value, 'startTime')
-                  }
+                  value={tolerance}
+                  onChange={e => setTolerance(e.target.value)}
                 />
               </div>
-              <div className='class-register__container__column__item__normal-input'>
-                <MaskInput
-                  type='text'
-                  mask='99'
-                  value={classList[1].periods}
-                  onChange={e => listHandleChange(1, e.target.value, 'periods')}
-                  placeholder='Períodos'
-                  name='mondayPeriods'
-                />
-              </div>
-            </div>
-            <div className='class-register__container__column__item'>
-              <Checkbox
-                checked={classList[2].isDaySelected}
-                onChange={() =>
-                  listHandleChange(
-                    2,
-                    !classList[2].isDaySelected,
-                    'isDaySelected'
-                  )
-                }
-                text='Quarta-feira'
-                name='wednesday'
+            </Row>
+          </div>
+          <div className='class-register__container__column'>
+            <div className='class-register__container__column__form-group'>
+              <Label
+                text='Dia da semana e início da aula'
+                htmlFor='dayOfWeek'
               />
-              <div className='class-register__container__column__item__special-input'>
-                <InputTime
-                  inputComponent={<Input />}
-                  value={classList[2].startTime}
-                  onChange={e =>
-                    listHandleChange(2, e.target.value, 'startTime')
+              <div className='class-register__container__column__item'>
+                <Checkbox
+                  checked={classList[0].isDaySelected}
+                  onChange={() =>
+                    listHandleChange(
+                      0,
+                      !classList[0].isDaySelected,
+                      'isDaySelected'
+                    )
                   }
+                  text='Segunda-feira'
+                  name='monday'
                 />
+                <div className='class-register__container__column__item__special-input'>
+                  <InputTime
+                    inputComponent={<Input />}
+                    value={classList[0].startTime}
+                    onChange={e =>
+                      listHandleChange(0, e.target.value, 'startTime')
+                    }
+                  />
+                </div>
+                <div className='class-register__container__column__item__normal-input'>
+                  <MaskInput
+                    type='text'
+                    mask='99'
+                    value={classList[0].periods}
+                    onChange={e =>
+                      listHandleChange(0, e.target.value, 'periods')
+                    }
+                    placeholder='Períodos'
+                    name='mondayPeriods'
+                  />
+                </div>
               </div>
-              <div className='class-register__container__column__item__normal-input'>
-                <MaskInput
-                  type='text'
-                  mask='99'
-                  value={classList[2].periods}
-                  onChange={e => listHandleChange(2, e.target.value, 'periods')}
-                  placeholder='Períodos'
-                  name='mondayPeriods'
+              <div className='class-register__container__column__item'>
+                <Checkbox
+                  checked={classList[1].isDaySelected}
+                  onChange={() =>
+                    listHandleChange(
+                      1,
+                      !classList[1].isDaySelected,
+                      'isDaySelected'
+                    )
+                  }
+                  text='Terça-feira'
+                  name='tuesday'
                 />
+                <div className='class-register__container__column__item__special-input'>
+                  <InputTime
+                    inputComponent={<Input />}
+                    value={classList[1].startTime}
+                    onChange={e =>
+                      listHandleChange(1, e.target.value, 'startTime')
+                    }
+                  />
+                </div>
+                <div className='class-register__container__column__item__normal-input'>
+                  <MaskInput
+                    type='text'
+                    mask='99'
+                    value={classList[1].periods}
+                    onChange={e =>
+                      listHandleChange(1, e.target.value, 'periods')
+                    }
+                    placeholder='Períodos'
+                    name='mondayPeriods'
+                  />
+                </div>
+              </div>
+              <div className='class-register__container__column__item'>
+                <Checkbox
+                  checked={classList[2].isDaySelected}
+                  onChange={() =>
+                    listHandleChange(
+                      2,
+                      !classList[2].isDaySelected,
+                      'isDaySelected'
+                    )
+                  }
+                  text='Quarta-feira'
+                  name='wednesday'
+                />
+                <div className='class-register__container__column__item__special-input'>
+                  <InputTime
+                    inputComponent={<Input />}
+                    value={classList[2].startTime}
+                    onChange={e =>
+                      listHandleChange(2, e.target.value, 'startTime')
+                    }
+                  />
+                </div>
+                <div className='class-register__container__column__item__normal-input'>
+                  <MaskInput
+                    type='text'
+                    mask='99'
+                    value={classList[2].periods}
+                    onChange={e =>
+                      listHandleChange(2, e.target.value, 'periods')
+                    }
+                    placeholder='Períodos'
+                    name='mondayPeriods'
+                  />
+                </div>
+              </div>
+              <div className='class-register__container__column__item'>
+                <Checkbox
+                  checked={classList[3].isDaySelected}
+                  onChange={() =>
+                    listHandleChange(
+                      3,
+                      !classList[3].isDaySelected,
+                      'isDaySelected'
+                    )
+                  }
+                  text='Quinta-feira'
+                  name='thursday'
+                />
+                <div className='class-register__container__column__item__special-input'>
+                  <InputTime
+                    inputComponent={<Input />}
+                    value={classList[3].startTime}
+                    onChange={e =>
+                      listHandleChange(3, e.target.value, 'startTime')
+                    }
+                  />
+                </div>
+                <div className='class-register__container__column__item__normal-input'>
+                  <MaskInput
+                    type='text'
+                    mask='99'
+                    value={classList[3].periods}
+                    onChange={e =>
+                      listHandleChange(3, e.target.value, 'periods')
+                    }
+                    placeholder='Períodos'
+                    name='mondayPeriods'
+                  />
+                </div>
+              </div>
+              <div className='class-register__container__column__item'>
+                <Checkbox
+                  checked={classList[4].isDaySelected}
+                  onChange={() =>
+                    listHandleChange(
+                      4,
+                      !classList[4].isDaySelected,
+                      'isDaySelected'
+                    )
+                  }
+                  text='Sexta-feira'
+                  name='friday'
+                />
+                <div className='class-register__container__column__item__special-input'>
+                  <InputTime
+                    inputComponent={<Input />}
+                    value={classList[4].startTime}
+                    onChange={e =>
+                      listHandleChange(4, e.target.value, 'startTime')
+                    }
+                  />
+                </div>
+                <div className='class-register__container__column__item__normal-input'>
+                  <MaskInput
+                    type='text'
+                    mask='99'
+                    value={classList[4].periods}
+                    onChange={e =>
+                      listHandleChange(4, e.target.value, 'periods')
+                    }
+                    placeholder='Períodos'
+                    name='mondayPeriods'
+                  />
+                </div>
               </div>
             </div>
-            <div className='class-register__container__column__item'>
-              <Checkbox
-                checked={classList[3].isDaySelected}
-                onChange={() =>
-                  listHandleChange(
-                    3,
-                    !classList[3].isDaySelected,
-                    'isDaySelected'
-                  )
-                }
-                text='Quinta-feira'
-                name='thursday'
-              />
-              <div className='class-register__container__column__item__special-input'>
-                <InputTime
-                  inputComponent={<Input />}
-                  value={classList[3].startTime}
-                  onChange={e =>
-                    listHandleChange(3, e.target.value, 'startTime')
-                  }
-                />
-              </div>
-              <div className='class-register__container__column__item__normal-input'>
-                <MaskInput
-                  type='text'
-                  mask='99'
-                  value={classList[3].periods}
-                  onChange={e => listHandleChange(3, e.target.value, 'periods')}
-                  placeholder='Períodos'
-                  name='mondayPeriods'
-                />
-              </div>
-            </div>
-            <div className='class-register__container__column__item'>
-              <Checkbox
-                checked={classList[4].isDaySelected}
-                onChange={() =>
-                  listHandleChange(
-                    4,
-                    !classList[4].isDaySelected,
-                    'isDaySelected'
-                  )
-                }
-                text='Sexta-feira'
-                name='friday'
-              />
-              <div className='class-register__container__column__item__special-input'>
-                <InputTime
-                  inputComponent={<Input />}
-                  value={classList[4].startTime}
-                  onChange={e =>
-                    listHandleChange(4, e.target.value, 'startTime')
-                  }
-                />
-              </div>
-              <div className='class-register__container__column__item__normal-input'>
-                <MaskInput
-                  type='text'
-                  mask='99'
-                  value={classList[4].periods}
-                  onChange={e => listHandleChange(4, e.target.value, 'periods')}
-                  placeholder='Períodos'
-                  name='mondayPeriods'
-                />
-              </div>
-            </div>
+          </div>
+        </Row>
+        <div className='class-register__container__buttons'>
+          <div className='class-register__container__buttons__button'>
+            <Button text='Cancelar' />
+          </div>
+          <div className='class-register__container__buttons__button'>
+            <Button text='Salvar' type='submit' onClick={() => null} />
           </div>
         </div>
       </div>
